80e0ecfadc09d7925f808e86e89363345bb514b1
minchina <minchina91@gmail.com>
2016-01-13 16:10:16 +0800

    add search navigator

--- index.ios.js
+++ index.ios.js
@@ -3,66 +3,81 @@
  * https://github.com/facebook/react-native
  */
 'use strict';
-
 var React = require('react-native');
 var {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  TextInput,
-  View,
-} = React;
+    AppRegistry,
+    StyleSheet,
+    Text,
+    TextInput,
+    View,
+    NavigatorIOS
+    } = React;
+
+var SearchScreen = React.createClass({
+    render: function () {
+        return (
+            <View style={styles.container}>
+                <Text style={styles.headline}>
+                    BookBrowser
+                </Text>
+                <Text style={styles.label}>
+                    Find books containing:
+                </Text>
+                <TextInput style={styles.textInput}
+                           placeholder="e.g. Javascript or Mobile"
+                           returnKeyType="search"
+                           enablesReturnKeyAutomatically={true}
+                           onEndEditing={event =>
+          console.log(event.nativeEvent.text)}/>
+            </View>
+        )
+    }
+});
 
 var BookBrowser = React.createClass({
-  render: function() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.headline}>
-          BookBrowser
-        </Text>
-        <Text style={styles.label}>
-          Find books containing:
-        </Text>
-        <TextInput style={styles.textInput}
-        placeholder="e.g. Javascript or Mobile"
-        returnKeyType="search"
-        enablesReturnKeyAutomatically={true}
-        onEndEditing={event =>
-          console.log(event.nativeEvent.text)}/>
-      </View>
-    );
-  }
+    render: function () {
+        return (
+            <NavigatorIOS
+                initialRoute={{component:SearchScreen,
+          title : 'Search'}}
+                style={styles.navContainer}
+                />
+        )
+    }
 });
 
 var styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    flexDirection: 'column',
-    backgroundColor:'#5AC8FA'
-  },
-  textInput:{
-    height: 40,
-    borderWidth: 0.5,
-    marginLeft: 60,
-    marginRight:60,
-    borderColor:'#8E8E93',
-    backgroundColor:'#FFF',
-    padding:8
-  },
-  headline:{
-    fontSize:36,
-    fontWeight:'bold',
-    color:'#FFF',
-    marginBottom :28
-  },
-  label:{
-    fontSize:24,
-    fontWeight:'normal',
-    color:'#FFF',
-    marginBottom:8
-  }
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        flexDirection: 'column',
+        backgroundColor: Math.random() > 0.5 ? '#5AC8FA' : '4CD964'
+    },
+    textInput: {
+        height: 40,
+        borderWidth: 0.5,
+        marginLeft: 60,
+        marginRight: 60,
+        borderColor: '#8E8E93',
+        backgroundColor: '#FFF',
+        padding: 8
+    },
+    headline: {
+        fontSize: 36,
+        fontWeight: 'bold',
+        color: '#FFF',
+        marginBottom: 28
+    },
+    label: {
+        fontSize: 24,
+        fontWeight: 'normal',
+        color: '#FFF',
+        marginBottom: 8
+    },
+    navContainer:{
+        flex:1
+    }
 });
 
 AppRegistry.registerComponent('BookBrowser', () => BookBrowser);
